9a2fa7c6b3bc9cf450cb3391e7a5fc5f23da518b
YessyNP <yessynindi@gmail.com>
2019-10-11 20:20:53 +0700

    penghapusan welcome activity karena splashscreen diubah ke main activity

---
+++ app/src/main/java/com/yessy/utsyessy/activities/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.yessy.utsyessy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yessy.utsyessy.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/yessy/utsyessy/activities/KebudayaanActivity.java
+++ app/src/main/java/com/yessy/utsyessy/activities/KebudayaanActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.yessy.utsyessy.R;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class KebudayaanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_kebudayaan);
     }
 }
