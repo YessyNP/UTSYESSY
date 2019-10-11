11868cb642306200fba9c73da416816cd34fde65
YessyNP <yessynindi@gmail.com>
2019-10-11 20:33:39 +0700

    membuat 3 tombol. sejarah,kebudayaan dan about

--- app/src/main/java/com/yessy/utsyessy/activities/MainActivity.java
+++ app/src/main/java/com/yessy/utsyessy/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.yessy.utsyessy.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.yessy.utsyessy.R;
 
@@ -12,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void HandleKebudayaanFragments(View view) {
+    }
+
+    public void handleAbout(View view) {
+    }
+
+    public void handleSejarahFragment(View view) {
     }
 }
