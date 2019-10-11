d581bcb83df260b2b78c3b34c9000506e93c1dac
YessyNP <yessynindi@gmail.com>
2019-10-12 00:02:12 +0700

    Mengaktifkan fragments agama,makanan dan keajaiban

--- app/src/main/java/com/yessy/utsyessy/activities/AboutActivity.java
+++ app/src/main/java/com/yessy/utsyessy/activities/AboutActivity.java
@@ -1,11 +1,15 @@
 package com.yessy.utsyessy.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.yessy.utsyessy.R;
+import com.yessy.utsyessy.fragments.AgamaFragments;
+import com.yessy.utsyessy.fragments.KeajaibanFragments;
+import com.yessy.utsyessy.fragments.MakananFragments;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -16,12 +20,23 @@ public class AboutActivity extends AppCompatActivity {
     }
 
     public void handleAgama(View view) {
-
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.about, new AgamaFragments(), "Agama_FRAGMENT");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     public void handleMakanan(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.about, new MakananFragments(), "Makanan_FRAGMENT");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     public void handleKeajaiban(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.about, new KeajaibanFragments(), "Keajaiban_FRAGMENT");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
