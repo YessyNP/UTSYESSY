443b5b07d5322ae6f49805e852f9d08e79c5c68d
YessyNP <yessynindi@gmail.com>
2019-10-11 23:39:21 +0700

    menambahkan button on click pada activity_about

--- app/src/main/java/com/yessy/utsyessy/activities/AboutActivity.java
+++ app/src/main/java/com/yessy/utsyessy/activities/AboutActivity.java
@@ -3,6 +3,7 @@ package com.yessy.utsyessy.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.yessy.utsyessy.R;
 
@@ -12,5 +13,15 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void handleAgama(View view) {
+
+    }
+
+    public void handleMakanan(View view) {
+    }
+
+    public void handleKeajaiban(View view) {
     }
 }
