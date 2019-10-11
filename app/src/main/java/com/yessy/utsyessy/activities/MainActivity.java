94d0556bf64cc9be6b56add1df69372230c8f306
YessyNP <yessynindi@gmail.com>
2019-10-11 21:55:35 +0700

    mengaktifkan button sejarah,kebudayaan dan about

--- app/src/main/java/com/yessy/utsyessy/activities/MainActivity.java
+++ app/src/main/java/com/yessy/utsyessy/activities/MainActivity.java
@@ -2,25 +2,36 @@ package com.yessy.utsyessy.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+
 import com.yessy.utsyessy.R;
+import com.yessy.utsyessy.fragments.SejarahFragments;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SejarahFragments sejarahFragments;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sejarahFragments = new SejarahFragments();
     }
 
     public void HandleKebudayaanFragments(View view) {
+        Intent intent = new Intent(this, KebudayaanActivity.class);
+        startActivity(intent);
     }
 
     public void handleAbout(View view) {
-    }
+        Intent intent = new Intent(this,AboutActivity.class);
+        startActivity(intent);
+          }
 
     public void handleSejarahFragment(View view) {
+        Intent intent = new Intent(this, SejarahActivity.class);
+        startActivity(intent);
+//        getSupportFragmentManager().beginTransaction().replace(this, SejarahActivity.class).commit();
     }
 }
