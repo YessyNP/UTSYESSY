6600d2a2c17c496f7a2ac2a1f2d78e68c5371898
YessyNP <yessynindi@gmail.com>
2019-10-11 19:54:23 +0700

    Splash dan welcome activity

---
+++ app/src/main/java/com/yessy/utsyessy/activities/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.yessy.utsyessy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yessy.utsyessy.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
