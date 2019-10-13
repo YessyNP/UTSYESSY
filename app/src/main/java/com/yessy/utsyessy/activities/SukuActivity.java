dc004d1e07c3bf3dd9d6c5fc82d8890fe16bfb7f
YessyNP <yessynindi@gmail.com>
2019-10-13 12:21:58 +0700

    membuat class suku dan mengubah isi dari class tersebut serta membuat button back

--- app/src/main/java/com/yessy/utsyessy/activities/SukuActivity.java
+++ app/src/main/java/com/yessy/utsyessy/activities/SukuActivity.java
@@ -2,22 +2,20 @@ package com.yessy.utsyessy.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.yessy.utsyessy.R;
 
-public class KebudayaanActivity extends AppCompatActivity {
+public class SukuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kebudayaan);
+        setContentView(R.layout.activity_suku);
     }
 
-    public void handleSukuActivity(View view) {
-        Intent intent = new Intent(this,SukuActivity.class);
-        startActivity(intent);
+    public void handleBack(View view) {
+        finish();
     }
 }
